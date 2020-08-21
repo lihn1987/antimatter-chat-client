b19c55716ce01524fe8d5f44fc6401667e785110
lihn1987 <lihn1987@gmail.com>
2020-08-21 22:36:37 +0800

    ipc test

---
+++ src/renderer/components/Main.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    dddddddddddddddd
+  </div>
+</template>
+
+<script>
+// import axios from 'axios';
+// import qs from 'qs'
+const { ipcRenderer } = require('electron')
+// var _this = null;
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+  },
+  mounted: function () {
+    //_this = this;
+    console.log('send test ping start')
+    console.log(ipcRenderer.sendSync('test', 'ping'))
+    console.log('send test ping end')
+  },
+  methods: {
+  }
+}
+</script> 
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+$header-height: 60px;
+$footer-height: 60px;
+$main-height: calc( 100vh - #{$header-height} - #{$footer-height});
+.header{
+  height:$header-height;
+  background:#106EFF;
+  line-height: $header-height;
+  font-size:28px;
+  color:#ffffff;
+}
+.main{
+  min-height: $main-height;
+}
+.foot{
+  height: $footer-height;
+  background:#106EFF;
+  line-height: $header-height;
+  font-size:28px;
+  color:#ffffff;
+}
+</style>
